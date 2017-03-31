da484f780a4c09e7a02e65d8c022e627c22c7fef
Kovila <k.o.v.i.11.d.a.y@gmail.com>
2017-03-31 04:08:47 +0200

    ajouter dossier client

--- client/client.ml
+++ client/client.ml
@@ -1,50 +1,134 @@
 (*ocamlc -o client.exe -thread -custom unix.cma threads.cma client.ml -cclib -lthreads -cclib -lunix *)
 
 (*TODO 
--methode pour parser protocol
+-methode pour parser protocol //DONE
 -recuperation des donnees entree par utilisateurs et conversion en protocol string
  *)
-let interfaceGraphique_to_String ()  =
-(*en attente de quelque chose de l'interface graphique
- et transformation en string et ecriture dans fd ET FLUSH*)
-  ;;
 class client serv p =
-object(s)
+object(self)
   val sock = ThreadUnix.socket Unix.PF_INET Unix.SOCK_STREAM 0
   val port_num = p
   val server = serv
                  
+  (*stocker placement, tirage*)
   method start () =
     let host = Unix.gethostbyname server in
     let h_addr = host.Unix.h_addr_list.(0) in
+    let sock_addr = Unix.ADDR_INET (h_addr, port_num) in
     Unix.connect sock sock_addr;
-    Thread th1 = Thread.create s#interfaceGraphique_A_Serveur (sock, sock_addr);
-    Thread th2 = Thread.create s#serveur_A_InterfaceGraphique (sock, sock_addr);
-    Thread.join th1;
-    Thread.join th2;
+    let th = Thread.create self#serveur_A_InterfaceGraphique sock in
+      self#interfaceGraphique_A_Serveur sock;
+      Thread.join th;
     Unix.close sock
                
   (*prendre entree de l'utilisateur et envoyer au serveur*)
-  method interfaceGraphique_A_Serveur s sa =
+  (*a remplacer par la fonction qui lance l'interface graphique 
+ici on simule l'interface graphique par le input stream du terminal*)
+  method interfaceGraphique_A_Serveur s =
     while true do
-      let si = (my_input_line Unix.stdin ^ "\n") in (*read form interface graphique*)
-      ignore (ThreadUnix.write s si 0 (String.length si));
-      interfaceGraphique_to_String s
-      else (Printf.printf "%s\n")
+      let protocol = ref "" in 
+      let usr_in= (self#lecture_proto Unix.stdin) in (*read form interface graphique*)
+      match usr_in with
+      | "CONNEXION" ->
+         let proto =  self#proto_connexion (self#lecture_proto Unix.stdin) in
+         ignore (ThreadUnix.write s proto 0 (String.length proto));
+      | "SORT" ->
+         protocol := self#proto_sort (self#lecture_proto Unix.stdin);
+      | "TROUVE" ->
+         protocol := self#proto_trouve ();
+         ignore (ThreadUnix.write s !protocol 0 (String.length !protocol));
+      | some -> ignore (Printf.printf "ERROR in interfaceGraphique_A_Serveur\n");
     done
 
   (*lit continuellement sur inputstream pour voir si serveur envoie des donnees,
    parse le protocol envoyer et fait appel aux methodes adequats*)
-  method serveur_A_InterfaceGraphique s sa =
+  method serveur_A_InterfaceGraphique s =
     
     while true do
-   
-      
+      let protocol_recu = self#lecture_proto s in
+      match protocol_recu with
+      | "BIENVENUE" ->
+         self#proto_bienvenue ((self#lecture_proto s), (self#lecture_proto s), (self#lecture_proto s));
+      | "CONNECTE" ->
+         self#proto_connecte ((self#lecture_proto s));
+      | "DECONNEXION" -> self#proto_connecte (self#lecture_proto s);
+      | "SESSION" -> self#proto_session ();
+      | "VAINQUEURE" -> self#proto_vainqueure (self#lecture_proto s);
+      | "TOUR" ->
+         self#proto_tour ((self#lecture_proto s), (self#lecture_proto s), (self#lecture_proto s));
+      | "RVALIDE" -> self#proto_rvalide ();
+      | "RINVALIDE" -> self#proto_rinvalide (self#lecture_proto s);
+      | "RATROUVE" -> self#proto_ratrouve (self#lecture_proto s);
+      | "RFIN" -> self#proto_rfin ();
+      | "SVALIDE" -> self#proto_svalide ();
+      | "SINVALIDE" -> self#proto_sinvalide (self#lecture_proto s);
+      | "SFIN" -> self#proto_sfin ();
+      | "BILAN" ->
+         self#proto_bilan ((self#lecture_proto s), (self#lecture_proto s),
+                           (self#lecture_proto s));
+         (*|some -> Printf.printf "ERROR in interfaceGraphique_A_Serveur\n";*)
     done
 
-  method proto_connexion s sa =
-    (*recuperer nom de l'utilisateur de l'interface graphique*)
-    let user = (my_input_line Unix.stdin) in
-    let protocol_cxn = "CONNEXION/" ^ user ^ "/" in
-    ignore (ThreadUnix.write s protocol_cnx 0 (String.length protocol_cnx));
+  method useless string = ();
+  method proto_bilan mot vainqueure score =
+    (*TODO*)
+    ();
+  method proto_sfin () =
+    (*TODO*)
+    ();
+  method proto_sinvalide raison =
+    (*TODO*)
+    ();
+  method proto_svalide () =
+    (*TODO*)
+    ();
+  method proto_rfin () =
+    (*TODO*)
+    ();
+  method proto_ratrouve placement user =
+    (*TODO*)
+    ();
+  method proto_rinvalide raison =
+    (*TODO*)
+    ();
+  method proto_rvalide () =
+    (*TODO*)
+    ();
+  method proto_tour placement tirage  =
+    (*TODO*)
+    ();
+  method proto_vainqueure bilan =
+    (*TODO*)
+    ();
+  method proto_session () =
+    (*TODO*)
+    ();
+  method proto_deconnexion user =
+    (*TODO*)
+    ();
+  method proto_bienvenue (placement, tirage, scores) =
+    (*TODO*)
+    "";
+  method proto_connecte user =
+    (*TODO*)
+    "";
+  method proto_connexion user =
+    "CONNEXION/" ^ user ^ "/"
+    ();
+  method proto_trouve () =
+    (*TODO*)
+    (*transformer tableau en string et former le protocol:
+     "TROUVE/" ^ placement ^ "/"
+     *)
+    ();
+  method proto_sort user =
+    "SORT/" ^ user ^ "/"
+    ;
+  method lecture_proto stream =
+    let s = "" and r = ref "" in
+    while (ThreadUnix.read stream s 0 1 > 0) && s.[0] <> '/' do
+      r := !r ^ s
+    done;
+    !r
+                                                           
 end;;
